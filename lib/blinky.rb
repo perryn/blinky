432975952f5ffd2b8f00f9d8630b9775f10e7656
Perryn Fowler <perryn.fowler@gmail.com>
2010-11-13 16:35:42 +1100

    rudimentary dsl for specifying device support

--- lib/blinky.rb
+++ lib/blinky.rb
@@ -6,21 +6,24 @@ require 'blinky/no_supported_devices_found'
 module Blinky
   class Indicator
     
-    def initialize(path = "#{File.dirname(__FILE__)}/../device_recipes/**/*.rb")
-      Dir[path].each { |f| require(f) }
+    def initialize(path = File.join(File.dirname(__FILE__), '..', 'device_recipes')) 
+      @recipes = Hash.new(:default => {})
+      Dir["#{path}/*/*.rb"].each { |f| require(f) }
+      instance_eval(File.read("#{path}/recipes.rb"))
       found_devices = []
       USB.devices.each do |device|
-        found_devices << device
-        begin
-           device_module_name =  "Blinky::Vendor_%04x::Product_%04x" % [device.idVendor, device.idProduct]
-           self.extend(eval(device_module_name))
+        found_devices << device  
+        matching_recipe = @recipes[device.idVendor][device.idProduct]   
+        if matching_recipe
+           self.extend(matching_recipe)
            @supported_device = device
-        rescue NameError
         end
       end
       raise NoSupportedDevicesFound.new(found_devices) unless @supported_device
       @handle = @supported_device.usb_open
     end
+    
+
     
     def success!
       begin
@@ -43,6 +46,11 @@ module Blinky
       end
     end
     
+ 
+
+    def add_recipe recipe_module, details
+       @recipes[details[:usb_vendor_id]] = {details[:usb_product_id] => recipe_module}
+    end
     
   end
 end
