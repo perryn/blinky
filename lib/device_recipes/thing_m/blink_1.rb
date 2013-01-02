c14f1408380206cca914a239399d3b274964e576
Perryn Fowler <perryn.fowler@gmail.com>
2013-01-02 13:32:43 +1100

    added support for ThingM blink(1)

---
+++ lib/device_recipes/thing_m/blink_1.rb
@@ -0,0 +1,64 @@
+module Blinky
+  module ThingM
+    module Blink1
+
+      def success!
+        stop 
+        set_colour("\x00\xFF\x00") 
+      end
+
+      def failure!
+        stop
+        set_flash_pattern("\xFF\x00\x00")      
+        play   
+      end
+
+      def building! 
+        stop 
+        set_colour("\x00\x00\xFF") 
+      end
+
+      def warning!
+        stop
+        set_colour("\xFF\xBE\x00") 
+      end
+
+      def off!
+        stop
+        set_colour("\x00\x00\x00") 
+      end
+
+      private
+      def set_colour colour
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01\x6E#{colour}\x00\x00\x00\x00", 1000)
+      end
+      
+      def play
+         @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01p\x01\x01\x00\x00\x00\x00\x00", 1000)
+      end
+      
+      def stop
+         @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01p\x00\x01\x00\x00\x00\x00\x00", 1000) 
+      end
+      
+      def set_flash_pattern colour
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P#{colour}\x00\x0A\x00\x00", 1000)
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x0A\x01\x00", 1000)
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x02\x00", 1000)
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x03\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x04\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x05\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x06\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x07\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x08\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x09\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x0A\x00", 1000) 
+        @handle.usb_control_msg(0x21, 0x09, (3 << 8) | 1, 0, "\x01P\x00\x00\x00\x00\x00\x0B\x00", 1000)
+      end
+      
+    end     
+  end
+end
+
+
+ 
--- lib/recipes.rb
+++ lib/recipes.rb
@@ -5,6 +5,10 @@ recipe DelcomEngineering::VisualIndicator::GenerationI,  {:usb_vendor_id => 0x0f
 recipe DelcomEngineering::VisualIndicator::GenerationII,  {:usb_vendor_id => 0x0fc5, 
                                                            :usb_product_id => 0xb080, 
                                                            :description => "USB Visual Signal Indicator Gen II"}
+                                                           
+recipe ThingM::Blink1,  {:usb_vendor_id => 0x27b8, 
+                         :usb_product_id => 0x01ed, 
+                         :description => "ThingM Blink(1)"}
                                                      
                                               
 #TODO - use treeop to allow a syntax like this 
